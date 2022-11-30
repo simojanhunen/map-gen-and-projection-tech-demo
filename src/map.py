6ee6cf2bdeccb694432b3b6b07df05d207ea7dc4
Simo Janhunen <simo.janhunen@gmail.com>
2022-11-30 22:47:34 +0200

    Add elementary tileset generation

--- src/map.py
+++ src/map.py
@@ -5,37 +5,70 @@ from settings import *
 from map_generator import generate_32x24_map
 
 
+class Tileset:
+    def __init__(self, file, size=(32, 32), margin=0, spacing=0) -> None:
+        self.file = file
+        self.size = size
+        self.margin = margin
+        self.spacing = spacing
+        self.image = pg.image.load(file)
+        self.rect = self.image.get_rect()
+        self.tiles = []
+        self.load()
+
+    def load(self):
+        self.tiles = []
+        x0 = y0 = self.margin
+        w, h = self.rect.size
+        dx = self.size[0] + self.spacing
+        dy = self.size[1] + self.spacing
+
+        for x in range(x0, w, dx):
+            for y in range(y0, h, dy):
+                tile = pg.Surface(self.size)
+                tile.blit(self.image, (0, 0), (x, y, *self.size))
+                self.tiles.append(tile)
+
+    def __str__(self):
+        return f"{self.__class__.__name__} file:{self.file} tile:{self.size}"
+
+    def get_tile(self, index):
+        return self.tiles[index]
+
+
 class Map:
     def __init__(self, game=None) -> None:
         self.game = game
         self.map_layout = None
         self.world_map = {}
-
+        self.floor_map = {}
+        self.tileset = Tileset("src/resources/tilemaps/dungeon.png")
         self.randomize()
-        self.populate_world_map()
+        self.populate_maps()
 
-    def populate_world_map(self):
+    def populate_maps(self):
         for j, row in enumerate(self.map_layout):
-            # For value in jth row and ith column add value to position (i, j) if it is not False
+            # For value in jth row and ith column add value to position (i, j) if wall else floor
             for i, value in enumerate(row):
-                if value:
+                if value == " ":
+                    pass
+                elif value:
                     self.world_map[(i, j)] = value
+                else:
+                    self.floor_map[(i, j)] = value
 
     def draw(self):
         if self.game:
-            for pos in self.world_map:
-                left_pos, top_pos = pos
-                left, top, width, height = (
-                    left_pos * BLOCK_SCALE,
-                    top_pos * BLOCK_SCALE,
-                    BLOCK_SCALE,
-                    BLOCK_SCALE,
+            for x, y in self.floor_map:
+                self.game.display.blit(
+                    self.tileset.get_tile(self.floor_map[x, y]),
+                    (x * BLOCK_SCALE, y * BLOCK_SCALE),
                 )
-                pg.draw.rect(
-                    surface=self.game.display,
-                    color="black",
-                    rect=(left, top, width, height),
-                    width=0,
+
+            for x, y in self.world_map:
+                self.game.display.blit(
+                    self.tileset.get_tile(self.world_map[x, y]),
+                    (x * BLOCK_SCALE, y * BLOCK_SCALE),
                 )
 
     def randomize(self):
@@ -43,8 +76,7 @@ class Map:
 
         # Convert string format to numbers
         map_layout_s = (
-            map_layout_s.replace(" ", "#")
-            .replace("<", ".")
+            map_layout_s.replace("<", ".")
             .replace(">", ".")
             .replace(".", "0")
             .replace("#", str(randint(1, 1)))
@@ -55,8 +87,9 @@ class Map:
         for i, row in enumerate(map_layout_s.split("\n")[:-1]):
             map_layout[i] = [*row]
 
-        # Convert string integers to integers
-        self.map_layout = [[int(i) for i in row] for row in map_layout]
+        self.map_layout = [
+            [int(i) if i != " " else i for i in row] for row in map_layout
+        ]
 
 
 if __name__ == "__main__":
--- src/settings.py
+++ src/settings.py
@@ -5,6 +5,9 @@ Stores all the game settings in one common location
 # Window and game display
 GAME_TITLE = "Unnamed raycaster 2D"
 DISPLAY_RESOLUTION = DISPLAY_WIDTH, DISPLAY_HEIGHT = 1024, 768
-BLOCK_SCALE = 32
+BLOCK_SCALE = 32  # 32x32px
 MAP_SIZE = MAP_COLS, MAP_ROWS = 32, 24
 FRAMES_PER_SECOND = 60
+
+# Tilesets
+TILESET_DUNGEON = "./resources/tilemaps/dungeon.png"
