17b92c1c6c1f1bd271f0d262888e2b8554a8117a
Simo Janhunen <simo.janhunen@gmail.com>
2022-12-11 19:56:55 +0200

    Add player collisions

--- src/player.py
+++ src/player.py
@@ -34,14 +34,26 @@ class Player:
             dx -= speed_sin
             dy += speed_cos
 
-        scale = PLAYER_SIZE_SCALE / self.game.dt
-        self.x += dx
-        self.y += dy
+        self.check_collisions(dx, dy)
 
         if keys[pg.K_LEFT]:
             self.angle -= PLAYER_ROTATION_SPEED * self.game.dt
         if keys[pg.K_RIGHT]:
             self.angle += PLAYER_ROTATION_SPEED * self.game.dt
+
+    def check_if_wall(self, x, y):
+        return (x, y) not in self.game.map.world_map
+
+    def check_collisions(self, dx, dy):
+        scale = PLAYER_SIZE_SCALE / self.game.dt
+        if self.check_if_wall(round(self.x + dx * scale), round(self.y)):
+            self.x += dx
+        if self.check_if_wall(round(self.x), round(self.y + dy * scale)):
+            self.y += dy
+
+        # print(
+        #     f"Player is colliding with floor: {self.check_if_wall(int(self.x), int(self.y))}"
+        # )
 
     def initialize(self) -> None:
         # Position
@@ -63,8 +75,8 @@ class Player:
         if self.initialized:
             scaled_x = self.x * BLOCK_SCALE + BLOCK_SCALE // 2
             scaled_y = self.y * BLOCK_SCALE + BLOCK_SCALE // 2
-            end_x = BLOCK_SCALE * math.cos(self.angle)
-            end_y = BLOCK_SCALE * math.sin(self.angle)
+            end_x = BLOCK_SCALE // 2 * math.cos(self.angle)
+            end_y = BLOCK_SCALE // 2 * math.sin(self.angle)
 
             self.map_exit = pg.draw.circle(
                 self.game.display,
@@ -73,7 +85,7 @@ class Player:
                     scaled_x,
                     scaled_y,
                 ),
-                16,
+                8,
             )
 
             pg.draw.line(
